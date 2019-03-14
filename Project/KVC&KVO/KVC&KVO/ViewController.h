f937a88f9e72253fb50a42b6a9333f129f45ecb0
yang <com.yangyangyangyang@gmail.com>
2019-03-14 16:34:02 +0800

    kvc&kvo

--- Project/KVC&KVO/KVC&KVO/ViewController.h
+++ Project/KVC&KVO/KVC&KVO/ViewController.h
@@ -1,12 +1,11 @@
 //
 //  ViewController.h
-//  GCD
+//  KVC&KVO
 //
 //  Created by yang on 2019/3/14.
 //  Copyright © 2019 XingYeTechnology. All rights reserved.
 //
 
-//GCD的理解代码 https://juejin.im/post/58fd55c08d6d81005898df46#heading-19
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
